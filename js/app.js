0b94211bb7c67af762be677c1282d48690c0e0cd
zahera <zahera.shehab@protonmail.com>
2026-06-28 14:16:54 +0300

    Set Up JavaScript Structure and Eache DOM Elements

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/*------------------------ Cached Element References ------------------------*/
+
+const cardsEls = document.querySelectorAll(".crds")
+const messageEl = document.querySelector("#message")
+const resetBtnEl = document.querySelector("#reset")
+
+/* console.log(cardsEls)
+console.log(messageEl)
+console.log(resetBtnEl) */
+
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
